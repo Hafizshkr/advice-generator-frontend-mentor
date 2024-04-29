3c8c037938c7bebb8c476067d62f722831122fee
Hafizshkr <103745630+Hafizshkr@users.noreply.github.com>
2024-04-29 14:57:27 +0800

    add loading feature and responsive for 375px

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 const btnQuote = document.getElementById("btn-quote");
 const idQuote = document.getElementById("id-quote");
 const textQuote = document.getElementById("text-quote");
+const loader = document.getElementById("loader");
 
 const url = "https://api.adviceslip.com/advice";
+
 async function getQuote() {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    showData(data);
+    showLoader();
+    setTimeout(() => {
+      showContent();
+      showData(data);
+    }, 100);
   } catch (error) {
     textQuote.innerText = `Data fetching error`;
     throw new error();
   }
+
+  function showLoader() {
+    textQuote.style.display = "none";
+    idQuote.style.display = "none";
+    loader.style.display = "block";
+  }
+
+  function showContent() {
+    textQuote.style.display = "block";
+    idQuote.style.display = "block";
+    loader.style.display = "none";
+  }
 }
 
 const showData = (data) => {
-  const { slip } = data;
-  console.log(slip);
   textQuote.innerText = `"${data.slip.advice}"`;
   idQuote.innerText = `Advice #${data.slip.id}`;
+  console.log(textQuote.innerHTML);
 };
 
 btnQuote.addEventListener("click", getQuote);
